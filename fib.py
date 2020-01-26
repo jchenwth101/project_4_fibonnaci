6d2e3f16d3fa4f110ee51f111ba3daae918e0784
Joel Chenoweth <jchenwth@yahoo.com>
2020-01-26 16:43:59 -0500

    Initial commit

---
+++ fib.py
@@ -0,0 +1,14 @@
+#this is my header.  there are many like it but this one is mine.  Joel Chenoweth
+#the year of our lord 2020, the month of January, day 26
+#the project is math fibonnacci sequence called without recursion
+def fib(n):
+        if n <= 2:
+            return 1
+        else:
+            return fib(n-1) + fib(n-2)
+term = 0
+term = fib(17)
+
+#print(term)
+
+
